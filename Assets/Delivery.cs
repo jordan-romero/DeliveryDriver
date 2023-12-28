65706ad7552aec5690394b07528c522a645e4473
Jordan Romero <jordan.t.romero@gmail.com>
2023-12-28 16:48:15 -0600

    Update delivery.cs to handle destroying packages after they have been picked up

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
+    [SerializeField] float destroyDelay = 1f;
      void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("OUCH... That hurt!");
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-            if(other.tag == "Package") 
+            if(other.tag == "Package" && !hasPackage) 
             {
                 Debug.Log("Package picked up!");
                 hasPackage = true;
+                Destroy(other.gameObject, destroyDelay);
             } else if (other.tag == "Customer" && hasPackage) 
             {
                 Debug.Log("Package delivered!");
