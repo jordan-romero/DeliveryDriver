4ae764d7abab3a9a79058a475ce8a545235e331d
Jordan Romero <jordan.t.romero@gmail.com>
2023-12-28 16:27:09 -0600

    Adds tags for Package and Customer adds if statements to check for tags and log messages

---
+++ Assets/Delivery.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+     void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("OUCH... That hurt!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+            if(other.tag == "Package") 
+            {
+                Debug.Log("Package picked up!");
+            } else if (other.tag == "Customer") 
+            {
+                Debug.Log("Package delivered!");
+            }
+    }
+}
