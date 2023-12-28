d7808b9f4e418d6498a903d3b1e81b4d910a39ae
Jordan Romero <jordan.t.romero@gmail.com>
2023-12-28 17:42:54 -0600

    Finishes up boost and bump speed changes

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -7,12 +7,20 @@ public class Driver : MonoBehaviour
     // these variables don't change so they do not need to be updated every frame 
     [SerializeField] float steerSpeed = 300f;
      [SerializeField] float moveSpeed =  20f;
-     [SerializeField] float slowSpeed =  15f;
+     [SerializeField] float slowSpeed =  1f;
     [SerializeField] float boostSpeed =  30f;
     // Start is called before the first frame update
-    void Start()
-    {
-        // transform.Rotate(0, 0, 90); 
+
+    void OnTriggerEnter2D(Collider2D other) {
+            if(other.tag == "Boost") {
+                moveSpeed = boostSpeed;
+            } 
+        }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.tag == "Bump") {
+            moveSpeed = slowSpeed;
+        }
     }
 
     // Update is called once per frame
@@ -21,9 +29,9 @@ public class Driver : MonoBehaviour
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime; 
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; 
         // variable is scoped to this update -- this allows this to update and calc every frame 
-
+    
         transform.Rotate(0, 0, -steerAmount); 
-        if
         transform.Translate(0, moveAmount, 0);
     }
+    
 }
