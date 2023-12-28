8d1b0c916545bf96822ece0a02c5f608dfa4bc62
Jordan Romero <jordan.t.romero@gmail.com>
2023-12-28 17:08:00 -0600

    Updates package color, changes color of car sprite on package pickup

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,6 +6,15 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
     [SerializeField] float destroyDelay = 1f;
+
+    [SerializeField] Color32 hasPackageColor = new Color32(16, 195, 18, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
      void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("OUCH... That hurt!");
     }
@@ -15,11 +24,14 @@ public class Delivery : MonoBehaviour
             {
                 Debug.Log("Package picked up!");
                 hasPackage = true;
+                spriteRenderer.color = hasPackageColor;
                 Destroy(other.gameObject, destroyDelay);
             } else if (other.tag == "Customer" && hasPackage) 
             {
                 Debug.Log("Package delivered!");
                 hasPackage = false;
+                spriteRenderer.color = noPackageColor;
             }
     }
 }
+
